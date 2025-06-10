2bf7be6effd1ec016a9fc3128ce057c806903d76
iHexCoder <93008806+iHexCoder@users.noreply.github.com>
2025-06-10 20:39:50 +0530

    Update app.py
    Add malicious bug test code

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+import os
+
 def main():
+    print("Malicious code executed!")
+    os.system("echo 'PoC: Bug Test Unauthorized deployment' > /tmp/malicious_bug.txt")
+    
+    print(f"Secret: {secret}")
     secret = "SUPER_SECRET_PASSWORD"
     print("Hello, CI/CD World!")
-    print(f"Secret: {secret}")
+    
 if __name__ == "__main__":
     main()
 
