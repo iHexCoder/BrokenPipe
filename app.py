41ccb16137351e20252d7e3840d63101fdf0f4ce
iHexCoder <93008806+iHexCoder@users.noreply.github.com>
2025-06-14 11:59:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 def main():
     print("Malicious code executed!")
     os.system("echo 'PoC: Bug Test Unauthorized deployment' > /tmp/malicious_bug.txt")
-    print("Kept Kalm Myself And Hacked Your Infra just using an CI/CD Pipeline")
+    print("Hacked Your Infra just using an CI/CD Pipeline")
     print("Tell Your Infra Boss that 0xOffensiveHex was here... and also tell your friends that update the pipeline...")
         
     secret = "SUPER_SECRET_PASSWORD"
